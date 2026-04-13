1a068d689d6a0017e8ee8abe40eb44fa2f7c7b42
leonardumale <leonardumale@gmail.com>
2026-04-12 21:16:36 -0700

    Implement nested loops to output index pairs
    Added nested loops to print pairs of indices based on input N.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int N;
-	cin >> N;
+    int N;
+    cin >> N;
 
-	// TODO
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = N - 1 - i; j < N; j++)
+        {
+            cout << i << " , " << j << endl;
+        }
+    }
+
+    return 0;
 }
